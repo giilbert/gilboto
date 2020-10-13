95cc9bdaab0f7d9f93d7895329c9593824361aeb
gil <gilbertzhang1208@gmail.com>
2020-10-13 16:48:35 -0400

    added info
    shows basic info about a guildmember

---
+++ commands/info.js
@@ -0,0 +1,34 @@
+const Discord = require("discord.js");
+
+
+let Config = JSON.parse(require("fs").readFileSync(require("path").join(__dirname, "../config.json")));
+
+let command = (msg, client) => {
+    
+    let mention = msg.mentions.members.first();
+
+    if (!mention) {
+        msg.reply("please mention a person you want to see info about");
+        return;
+    }
+
+    let embed = new Discord.MessageEmbed()
+        .setTitle(mention.user.tag + " info")
+        .setColor(Config.theme.mainColor)
+        .addField("**Tag**", mention.user.tag)
+        .addField("**Creation Date**", mention.joinedAt)
+        .addField("**Id**", mention.id)
+        .addField("**Permissions**", mention.permissions.toArray().join(", "))
+        .addField("**Bitfield**", mention.permissions.bitfield)
+
+    msg.channel.send(embed);
+}
+
+let help = (msg) => {
+
+}
+
+exports.register = () => {
+    require("./execute").registerCommand("info", command)
+    require("./help").registerHelp("info", help)
+}
--- commands/registerCommands.js
+++ commands/registerCommands.js
@@ -2,6 +2,9 @@ exports.all = () => {
     require("./ping").register();
     require("./help").register();
     require("./invite").register();
+
+    require("./info").register();
+    //require("./serverinfo").register(); todo
     
     require("./moderation/kick").register();
     require("./moderation/ban").register();
